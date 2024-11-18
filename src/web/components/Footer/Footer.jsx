6804d9dba327bad1b810e929cd3d26a32d5e31f4
ernnies <ernestnnamdie@gmail.com>
2024-11-18 22:03:38 +0100

    chain files

--- src/web/components/Footer/Footer.jsx
+++ src/web/components/Footer/Footer.jsx
@@ -1,35 +1,11 @@
-"use client"
+import './Footer.css'
 
-import {
-  Toast,
-  ToastClose,
-  ToastDescription,
-  ToastProvider,
-  ToastTitle,
-  ToastViewport,
-} from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast"
-
-export function Toaster() {
-  const { toasts } = useToast()
-
+const Footer = () => {
   return (
-    <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        )
-      })}
-      <ToastViewport />
-    </ToastProvider>
+    <div className='footer'>
+      <p>Copyright @ 2024, Cryptoplace - All Right Reserved.</p>
+    </div>
   )
 }
+
+export default Footer
